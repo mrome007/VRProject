6d702d049626d14b409cd6beff71968280929682
mrome007 <mrome007@ucr.edu>
2018-06-05 06:48:15 -0700

    Finish gaze system

--- Assets/Scripts/GazeSystem.cs
+++ Assets/Scripts/GazeSystem.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class GazeSystem : MonoBehaviour 
 {
+	[SerializeField]
+	private GameObject reticle;
+
+	private Color inactiveReticleColor = Color.white;
+	private Color activeReticleColor = Color.green;
+
+	private GazeableObject currentGazeObject;
+	private GazeableObject currentSelectableObject;
+
+	private RaycastHit lastHit;
+
+	private void Start()
+	{
+		SetReticleColor(inactiveReticleColor);
+	}
+
 	private void Update()
 	{
 		ProcessGaze();
+		CheckForInput(lastHit);
 	}
 	
 	public void ProcessGaze()
@@ -16,7 +33,72 @@ public class GazeSystem : MonoBehaviour
 
 		if(Physics.Raycast(ray, out hit))
 		{
+			var hitObject = hit.collider.gameObject;
+			var gaze = hitObject.GetComponent<GazeableObject>();
 
+			if(gaze != null)
+			{
+				if(gaze != currentGazeObject)
+				{
+					ClearCurrentObject();
+					currentGazeObject = gaze;
+					gaze.OnGazeEnter(hit);
+					SetReticleColor(activeReticleColor);
+				}
+				else
+				{
+					currentGazeObject.OnGaze(hit);
+				}
+			}
+			else
+			{
+				ClearCurrentObject();
+			}
+
+			lastHit = hit;
+		}
+		else
+		{
+			ClearCurrentObject();
+		}
+	}
+
+	private void SetReticleColor(Color reticleColor)
+	{
+		var renderer = reticle.GetComponent<Renderer>();
+		if(renderer != null)
+		{
+			renderer.material.SetColor("_Color", reticleColor);
+		}
+	}
+
+	private void CheckForInput(RaycastHit hit)
+	{
+		if(Input.GetMouseButtonDown(0) && currentGazeObject != null)
+		{
+			currentSelectableObject = currentGazeObject;
+			currentSelectableObject.OnPress(hit);
+		}
+
+		if(Input.GetMouseButton(0) && currentSelectableObject != null)
+		{
+			currentSelectableObject.OnHold(hit);
+		}
+
+		if(Input.GetMouseButtonUp(0) && currentSelectableObject != null)
+		{
+			currentSelectableObject.OnRelease(hit);
+			currentSelectableObject = null;
+		}
+	}
+
+	private void ClearCurrentObject()
+	{
+		if(currentGazeObject != null)
+		{
+			currentGazeObject.OnGazeExit();
+			SetReticleColor(inactiveReticleColor);
+			currentGazeObject = null;
 		}
 	}
 }
---
+++ Assets/Scripts/GazeableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeableObject : MonoBehaviour 
+{
+	public virtual void OnGazeEnter(RaycastHit hit)
+	{
+		Debug.Log("Gaze entered on " + gameObject.name);
+	}
+
+	public virtual void OnGaze(RaycastHit hit)
+	{
+		Debug.Log("Gaze hold on " + gameObject.name);
+	}
+
+	public virtual void OnGazeExit()
+	{
+		Debug.Log("Gaze exited on " + gameObject.name);
+	}
+
+	public virtual void OnPress(RaycastHit hit)
+	{
+		Debug.Log("Button Press");
+	}
+
+	public virtual void OnHold(RaycastHit hit)
+	{
+		Debug.Log("Button Hold");
+	}
+
+	public virtual void OnRelease(RaycastHit hit)
+	{
+		Debug.Log("Button Released");
+	}
+}
