70a267adce3db5ada8b482422b7e5796228456ca
mrome007 <mrome007@ucr.edu>
2018-06-05 06:05:37 -0700

    Beginning of gaze system.

---
+++ Assets/Scripts/GazeSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeSystem : MonoBehaviour 
+{
+	private void Update()
+	{
+		ProcessGaze();
+	}
+	
+	public void ProcessGaze()
+	{
+		var ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+
+		if(Physics.Raycast(ray, out hit))
+		{
+
+		}
+	}
+}
