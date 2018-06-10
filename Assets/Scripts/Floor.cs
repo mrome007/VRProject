9306098b5885eb8b14d5f98718d72e7a606e818c
mrome007 <mrome007@ucr.edu>
2018-06-09 17:08:11 -0700

    Add Teleport Functionality.

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : GazeableObject
+{
+	public override void OnPress(RaycastHit hit)
+	{
+		base.OnPress(hit);
+
+		if(Player.Instance.ActiveMode == InputMode.TELEPORT)
+		{
+			var position = hit.point;
+			position.y = Player.Instance.transform.position.y;
+			Player.Instance.transform.position = position;
+		}
+	}
+}
